4c8ada421b85411410623e10248239c0258ac390
Arshad-97 <agmarshad97@gmail.com>
2020-10-25 12:59:45 +0530

    DB connect and fetch data and show that

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h2>PHP-CRUD : Display data in php</h2>.
+            <tr>
+                <?php 
+                    $connection = mysqli_connect("localhost","root","");
+                    $db = mysqli_select_db($connection, 'php-crud-1');
+                    $query = "SELECT * FROM students";
+                    $query_run = mysqli_query($connection,$query);
+                ?>
+
+                <table class="table table-bordered" style="background-color: White;">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Contact </th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+
+                    </thead>
+                    <?php
+                    if($query_run){
+                        while($row = mysqli_fetch_array($query_run))
+                        {
+                            ?>
+                    <tbody>
+                        <tr>
+                            <th><?php  echo $row['id'];?></th>
+                            <th><?php echo $row['fname'];?></th>
+                            <th><?php echo $row['lname']; ?></th>
+                            <th><?php  echo $row['contact'];?></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </tbody>
+
+                <?php
+                        }
+
+                    }
+                    else{
+                        echo "Record not found";
+                    }
+
+                ?>
+            </tr>
+
+        </div>
+
+        </table>
+    </div>
+</body>
+
+</html>
